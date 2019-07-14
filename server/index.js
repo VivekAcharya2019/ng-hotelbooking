342de80dcb9a17e620f058b3c99f0576b129db4e
vivek acharya <vivekacharya2019@outlook.com>
2019-07-14 19:01:53 +0530

    Initial server Get method

--- server/index.js
+++ server/index.js
@@ -6,3 +6,8 @@ const PORT=process.env.PORT || 3001;
 app.listen(PORT,()=>{
     console.log("server running");
 })
+
+app.get('/rentals',function(req,res){
+    res.json({'success':true});
+    });
+
