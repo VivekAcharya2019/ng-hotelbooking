cdf7a3f86dfc6016d4e99dce1e3a86b5d8061bb2
vivek acharya <vivekacharya2019@outlook.com>
2019-07-14 20:25:48 +0530

    Initial Mongo setup, mongoose npm, connect via driver URL

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    DB_URI:'mongodb+srv://Test:testtest@cluster0-koa7n.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
+const mongoose = require('mongoose');
+const config=require('./config/dev');
 
 const app = express();
+mongoose.connect(config.DB_URI,{ useNewUrlParser: true });
 
 const PORT=process.env.PORT || 3001;
 app.listen(PORT,()=>{
