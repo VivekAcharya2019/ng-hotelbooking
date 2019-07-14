a244ca325d53cb63298d2c2ed936638e073b82e9
vivek acharya <vivekacharya2019@outlook.com>
2019-07-14 22:46:39 +0530

    insert data to Mongo DB, using Express router outside the main server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 const express=require('express');
 const mongoose = require('mongoose');
 const config=require('./config/dev');
+const Rental = require('./models/rental');
+const FakeDb = require('./fake-db');
+
+const rentalRouter = require('./routes/rentals');
 
 const app = express();
-mongoose.connect(config.DB_URI,{ useNewUrlParser: true });
+mongoose.connect(config.DB_URI,{ useNewUrlParser: true }).then(()=>{
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+});
 
 const PORT=process.env.PORT || 3001;
 app.listen(PORT,()=>{
     console.log("server running");
 })
 
-app.get('/rentals',function(req,res){
-    res.json({'success':true});
-    });
+app.use('/api/v1/rentals/',rentalRouter);
+// app.get('/rentals',function(req,res){
+//     res.json({'success':true});
+//     });
 
